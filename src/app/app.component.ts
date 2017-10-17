7fe24e22577ff7246b9d0ce67975d9524f3c872b
Manuel Gonzalez <manuelarvelo19@gmail.com>
2017-10-17 01:04:50 -0400

    app finalizada

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  items:any = [];
+  taxistaSelecionado:any = {};
   title = 'app';
+  lat = 18.4967726;
+  lng = -69.99436039999999;
+  siguiendo:boolean =  false;
+  constructor(private afDB: AngularFireDatabase){
+    afDB.list('/usuarios').subscribe((taxistas)=>{
+      this.items = taxistas;
+      if(this.siguiendo){
+        for(let taxista of taxistas){
+          if(taxista.$key === this.taxistaSelecionado.$key){
+            this.lat = taxista.lat;
+            this.lng = taxista.lng;
+          }
+        }
+      }
+    })
+  }
+  seguir_taxista(taxista: any){
+    this.lat = taxista.lat;
+    this.lng = taxista.lng;
+    this.taxistaSelecionado = taxista;
+    this.siguiendo = true;
+  }
+  dejar_seguir(){
+    this.siguiendo = false;
+    this.taxistaSelecionado = {};
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AgmCoreModule } from '@agm/core';
 
 import { AppComponent } from './app.component';
+
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyAckAjnVAbFfvcFVmIoWCBxd6oZgi5YGcQ",
+    authDomain: "tracker-455be.firebaseapp.com",
+    databaseURL: "https://tracker-455be.firebaseio.com",
+    projectId: "tracker-455be",
+    storageBucket: "tracker-455be.appspot.com",
+    messagingSenderId: "284184293474"
+};
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AgmCoreModule.forRoot({
+     apiKey: 'AIzaSyDmOaIIzhvAc-mSah3ize6KCoidxBPDzhA'
+   }),
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
